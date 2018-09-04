d913e198f09962c6e2b336a5dffc5be6eebe84dd
ligeng910118 <31567614+ligeng910118@users.noreply.github.com>
2018-09-04 17:48:37 +0800

    9-4 17:48

---
+++ java/Frog.java
@@ -0,0 +1,14 @@
+class Characteristic
+{
+	private String s;
+	Characteristic(String s)
+	{
+		this.s = s;
+		System.out.println("creating Characteristic " + s);
+	}
+
+	protected void dispose()
+	{
+		
+	}
+}

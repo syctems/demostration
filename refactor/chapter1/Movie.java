e847c2c2cf6eceb3cc70265d46e4358d6ef92127
ligeng910118 <31567614+ligeng910118@users.noreply.github.com>
2018-11-06 11:02:24 +0800

    11-6 11:02

--- refactor/chapter1/Movie.java
+++ refactor/chapter1/Movie.java
@@ -6,21 +6,35 @@ public class Movie
 
 	private String _title;
 	private int _priceCode;
+	private Price _price;
 
 	public Movie(String title,int priceCode)
 	{
 		_title = title;
-		_priceCode = priceCode;
+		setPriceCode(priceCode);
 	}
 
 	public int getPriceCode()
 	{
-		return _priceCode;
+		return _price.getPriceCode();
 	}
 
 	public void setPriceCode(int arg)
 	{
-		_priceCode = arg;
+		switch(arg)
+		{
+			case REGULAR:
+				_price = new RegularPrice();
+				break;
+			case CHILDRENS:
+				_price = new ChildrensPrice();
+				break;
+			case NEW_RELEASE:
+				_price = new NewReleasePrice();
+				break;
+			default :
+				throws new IllegalArgumentException("incorrect price code!");
+		}
 	}
 
 	public String getTitle()
@@ -28,25 +42,73 @@ public class Movie
 		return _title;
 	}
 
-	private double charget()
+	public double charget(int daysRented)
 	{
-			double result = 0;
-			switch(each.getMovie().getPriceCode())
-			{
-				case Movie.REGULAR:
-					result += 2;
-					if(each.getDaysRented() > 2)
-						result += (each.getDaysRented() - 2) * 1.5;
-					break;
-				case Movie.NEW_RELEASE:
-					result += each.getDaysRented() * 3;
-					break;
-				case Movie.CHILDRENS:
-						result += 1.5;
-						if(each.getDaysRented() > 3)
-							result += (each.getDaysRented() - 3) * 1.5;
-					break;
-			}
-			return result;
+		return _price.charget(daysRented);
+	}
+
+	public int getFrequentRenterPoints(int daysRented)
+	{
+		return _price.getFrequentRenterPoints(daysRented);
+	}
+}
+
+abstract class Price
+{
+	abstract int getPriceCode();
+	abstract double charget(int daysRented);
+	int getFrequentRenterPoints(int daysRented)
+	{
+			return 1;
+	}
+}
+
+class ChildrensPrice extends Price
+{
+	int getPriceCode()
+	{
+		return Movie.CHILDRENS;
+	}
+
+	double charget(int daysRented)
+	{
+		double result = 1.5;
+		if(daysRented > 3)
+			result += (daysRented - 3) * 1.5;
+		return result;
+	}
+}
+
+class NewReleasePrice extends Price
+{
+	int getPriceCode()
+	{
+		return Movie.NEW_RELEASE;
+	}
+
+	double charget(int daysRented)
+	{
+		return daysRented * 3;
+	}
+
+	int getFrequentRenterPoints(int daysRented)
+	{
+		return (daysRented > 1) ? 2 : 1;
+	}
+}
+
+class RegularPrice extends Price
+{
+	int getPriceCode()
+	{
+		return Movie.REGULAR;
+	}
+
+	double charget(int daysRented)
+	{
+		double result = 2;
+		if(daysRented > 2)
+			result += (daysRented - 2) * 1.5;
+		return result;
 	}
 }
--- refactor/chapter1/Rental.java
+++ refactor/chapter1/Rental.java
@@ -20,33 +20,13 @@ class Rental
 	}
 
 
-	private double charget()
+	public double charget()
 	{
-			double result = 0;
-			switch(getMovie().getPriceCode())
-			{
-				case Movie.REGULAR:
-					result += 2;
-					if(getDaysRented() > 2)
-						result += (getDaysRented() - 2) * 1.5;
-					break;
-				case Movie.NEW_RELEASE:
-					result += getDaysRented() * 3;
-					break;
-				case Movie.CHILDRENS:
-						result += 1.5;
-						if(getDaysRented() > 3)
-							result += (getDaysRented() - 3) * 1.5;
-					break;
-			}
-			return result;
+		return _movie.charget(_daysRented);
 	}
 
-	private int getFrequentRenterPoints()
+	public int getFrequentRenterPoints()
 	{
-		if((getMovie().getPriceCode() == Movie.NEW_RELEASE)&& getDaysRented() > 1)
-			return 2;
-		else
-			return 1;
+		return _movie.getFrequentRenterPoints(_daysRented);
 	}
 }

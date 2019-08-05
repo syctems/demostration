0f45f0bf487af2638a3b60e60df9f856483f57a1
Li Geng <2316558806@qq.com>
2019-08-05 17:34:00 +0800

    8-5 17:33

--- ant/source/newFold/Hello.java
+++ ant/source/newFold/Hello.java
@@ -1,6 +1,11 @@
 package newFold;
 public class Hello{
-	public static void firstAction() {
+	public static void firstAction() 
+	{
 		System.out.println("hello");
+	}
+	public static void main(String[] args)
+	{
+		firstAction();	
 	}
 }
